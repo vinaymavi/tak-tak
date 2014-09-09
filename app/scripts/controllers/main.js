b2736e059b03fcb978b323e1cd365fd3855ad5fe
mavi <vinaymavi@gmail.com>
2014-09-09 12:08:57 +0530

    Event registerd

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,28 +9,9 @@
  */
 angular.module('persistentInterViewApp')
   .controller('MainCtrl', function ($scope,imageDesc) {
-
-  $scope.myInterval = 5000;
-  var slides = $scope.slides = [
-  								{image:'http://storage.googleapis.com/vinaymavi/garrotxa.jpg'},
-  								{image:'http://storage.googleapis.com/vinaymavi/pvi-header.jpg'},
-  								{image:'http://storage.googleapis.com/vinaymavi/treefrog1.jpg'},
-  								{image:'http://storage.googleapis.com/vinaymavi/tumblr_static_slide2_butterfly.jpg'}
-  								];
-  // ImageDesc Object Return from $resource.
   $scope.imagesDesc =  imageDesc.get();
   $scope.curImgDesc;
-  $scope.$on('SLIDE_CHANGE',function(event,index){
-  	  	
-  	$scope.curImgDesc = $scope.imagesDesc[$scope.slides[index].image];
-  	console.log($scope.curImgDesc);
-  })
-  $scope.submitForm = function(isValid) {
-		// check to make sure the form is completely valid
-		if (isValid) { 
-			alert('our form is amazing');
-		}
-
-	}
-
+    $scope.keypress = function(event){
+      console.log(event);
+    }
   });
